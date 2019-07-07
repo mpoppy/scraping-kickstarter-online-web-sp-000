d67bd55d9cea537ba3686726a9a4bb099de261e2
Marc Poppy <poppy.marc@gmail.com>
2019-07-07 22:47:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,3 +11,4 @@ end
 create_project_hash
 #projects: kickstarter.css("li.project_grid_4") selector for projects
 # title: project.css("h2.bbcard_name strong a").text
+#image link: project.css("div.project-thumbnail a img").attribute("src").value
